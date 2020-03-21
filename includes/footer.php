7f56bd957c23848e3db8a3957f63e232c61b844c
unknown <infolintermediaire@gmail.com>
2020-03-21 13:47:35 +0100

    initialisation

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+  <?php if($config['jQuery'] === true) : ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <?php endif;?>
+  <script src="<?= linter_url();?>js/main.js"></script>
+</body>
+</html>
+<!-- Réalisé par lintermediaire -->
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include('includes/header.php');?>
+<div class="container" data-url="<?= linter_url();?>" data-version="<?= $config['version'];?>">
+  <section>
+    <div class="flex">
+      <h1>Site en construction</h1>
+      <a href="" class="btn">Contact !</a>
+      <img src="https://unsplash.it/g/64/64" alt="this" title="this"/>
+    </div>
+  </section>
+</div>
+<?php include('includes/footer.php');?>
